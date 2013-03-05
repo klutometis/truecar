ac7676d2a79923d2a5f259d672ea6b0d350f97a1
Peter Danenberg <pcd@roxygen.org>
2013-03-05 14:01:06 -0800

    Details

--- truecar/static/index.js
+++ truecar/static/index.js
@@ -12,6 +12,27 @@ $(function() {
       event.preventDefault();
       $("#search").val(ui.item.label);
       $("#details").empty()
-      $("#details").append($("<dt>").append("harro")).append($("<dd>").append("thar"))
+      $.get("details/" + ui.item.value + "/",
+           function (vehicle) {
+             var flag;
+             var default_image = "http://img.truecar.com/colorid_images/v1/None/175x90/f3q";
+
+             if (vehicle.flag)
+               flag = "Yes"
+             else
+               flag = "No"
+             
+             if (!vehicle.image)
+               vehicle.image = default_image
+
+             $("#details").append($("<dt>").append("Make")).append($("<dd>").append(vehicle.make))
+             $("#details").append($("<dt>").append("Model")).append($("<dd>").append(vehicle.model))
+             $("#details").append($("<dt>").append("Body")).append($("<dd>").append(vehicle.body))
+             $("#details").append($("<dt>").append("Flag")).append($("<dd>").append(flag))
+             $("#details").append($("<dt>").append("Year")).append($("<dd>").append(vehicle.year))
+             $("#details").append($("<dt>").append("MSRP")).append($("<dd>").append(vehicle.MSRP))
+             $("#details").append($("<dt>").append("Details")).append($("<dd>").append("See ").append($("<a />", {href: vehicle.details}).append("here")).append("."))
+             $("#details").append($("<dt>").append("Image")).append($("<dd>").append($("<img />", {src: vehicle.image})))
+           })
     }});
 })
