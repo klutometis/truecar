a6622c38b8c2d9bc10e10fda5d7843fabd4b0a48
Peter Danenberg <pcd@roxygen.org>
2013-03-04 17:46:40 -0800

    Use a custom source.

--- truecar/static/index.js
+++ truecar/static/index.js
@@ -1,5 +1,11 @@
 $(function() {
   $("#search").autocomplete({
-    source: "search/"
-  });
+    // source: "search/"
+    source: function(request, response) {
+      $.get("search/",
+            {term: request.term},
+            function(data) {
+              response(data);
+            });
+    },
 })
