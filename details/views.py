11a356d3efcdb9adf0fed421c8643765a23d24d8
Peter Danenberg <pcd@roxygen.org>
2013-03-05 13:57:39 -0800

    Manual JSON serialization

--- details/views.py
+++ details/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 from django.utils import simplejson as json
 from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
 
 from search.models import Vehicle
 
 # http://stackoverflow.com/questions/757022/how-do-you-serialize-a-model-instance-in-django
 def details(request, vehicle_id):
     vehicle = Vehicle.objects.get(pk=vehicle_id)
-    return HttpResponse(json.dumps(serializers.serialize('json', [ vehicle, ])),
+    return HttpResponse(json.dumps({'make': vehicle.make,
+                                    'model': vehicle.model,
+                                    'body': vehicle.body,
+                                    'flag': vehicle.flag,
+                                    'year': vehicle.year,
+                                    'MSRP': vehicle.MSRP,
+                                    'details': vehicle.details,
+                                    'image': vehicle.image}),
                         content_type='application/json')
